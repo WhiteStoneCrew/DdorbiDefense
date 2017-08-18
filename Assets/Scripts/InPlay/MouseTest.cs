95247881260b8c53eb54e2dde9292c0ffb929e2a
Kangsungmin <belo7@naver.com>
2017-08-18 12:28:42 +0900

    StageEdit
    스테이지1~4
    웨이브 적용

---
+++ Assets/Scripts/InPlay/MouseTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTest : MonoBehaviour {
+
+    public Ray ray;
+    public RaycastHit hitInfo;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                //Debug.Log(hitInfo.transform.gameObject.name); //gameObject name
+                if (hitInfo.transform.gameObject.name == "Enemy_Android(Clone)")
+                {
+                    hitInfo.transform.GetComponent<AndroidBot>().TakeDamage(2);
+                }
+                
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/InPlay/UI/UIEvent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEvent : MonoBehaviour {
+    public GameObject Menu;
+    bool pause;
+	// Use this for initialization
+	void Start () {
+        pause = false;
+	}
+
+    public void StopBtn()
+    {
+        if (pause)
+        {
+            pause = false;
+            Menu.SetActive(false);
+            Time.timeScale = 1;
+        }
+        else
+        {
+            pause = true;
+            Menu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        
+    }
+
+    public void GoStageMenu()
+    {
+        Application.LoadLevel("StageMenu");
+    }
+}
